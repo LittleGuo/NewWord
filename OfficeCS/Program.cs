99cd2ec5b2e6d3b4e793448c9bed47d5fe99b5d9
Little Pan <littleoldpan@163.com>
2016-06-12 22:37:25 +0800

    office cs
    c# of office

---
+++ OfficeCS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+
+namespace OfficeCS
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
